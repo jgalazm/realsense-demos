96c4c1ffc0766cae1f906f9507f7e53b5e6b5d88
Jose Galaz <jdgalaz@uc.cl>
2018-04-10 15:00:49 -0300

    add matplotlib animation

--- demo2/demo_numpy.py
+++ demo2/demo_numpy.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # First import the library
 import pyrealsense2 as rs
@@ -8,17 +9,29 @@ import pyrealsense2 as rs
 pipeline = rs.pipeline()
 pipeline.start()
 
+f = plt.figure()
+ax = f.add_subplot(111)
+frames = pipeline.wait_for_frames()
+depth = frames.get_depth_frame()
+depth_data = depth.as_frame().get_data()
+np_image = np.asanyarray(depth_data)
+pmesh = ax.imshow((np_image)/100,vmin=0.0,vmax=100.0,cmap=plt.cm.gray)
+f.colorbar(pmesh)
 
-while True:
-    # This call waits until a new coherent set of frames is available on a device
-    # Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
+def animate(iter):
     frames = pipeline.wait_for_frames()
     depth = frames.get_depth_frame()
     depth_data = depth.as_frame().get_data()
     np_image = np.asanyarray(depth_data)
-    np.set_printoptions(precision=3)
-    print(np_image)
-    plt.pcolormesh(np.flipud(np_image)/10,vmin=0.0,vmax=1000.0,cmap=plt.cm.jet)
-    plt.colorbar()
-    #plt.savefig('asdf.png')
-    plt.show()
+    pmesh.set_array(np_image/100)
+
+print('asdf')
+anim = animation.FuncAnimation(f,animate,frames=1000,interval=50,blit=False,repeat=False)
+plt.show()
+
+# while True:
+#     # This call waits until a new coherent set of frames is available on a device
+#     # Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
+    
+#     ax.imshow((np_image)/100,vmin=0.0,vmax=100.0,cmap=plt.cm.jet)
+#     plt.show()
